d336fbba55444a0ee1c172662b7cd8e681b58716
x-Zeiku-x <bjarat.90@gmail.com>
2020-06-26 17:06:25 -0400

    Se corrige un error que no listaba bien (necesario que actualizen a esta version)

--- PostgreSQL/LLenarComuna.php
+++ PostgreSQL/LLenarComuna.php
@@ -2,7 +2,6 @@
 
 include_once '../PostgreSQL/ConexionBD.php';
 $ciudad = $_POST['id_ciudad'];
-echo $ciudad;
 $consultaSQL = ConexionBD::abrirConexion()->prepare("SELECT id_comuna, nombre_comuna FROM comuna WHERE id_ciudad = ?");
 $consultaSQL->bindParam(1, $ciudad);
 //$consultaSQL->setFetchMode(PDO::FETCH_ASSOC);
@@ -12,7 +11,7 @@ $ResultadoSQL = $consultaSQL->fetchAll(PDO::FETCH_ASSOC);
 $html = "<option value='0'>" . 'Seleccione una comuna...' . "</option>";
 
 foreach ($ResultadoSQL as $fila) {
-    $html = "<option value='" . $fila['id_comuna'] . "'>" . $fila['nombre_comuna'] . "</option>";
+    $html .= "<option value='" . $fila['id_comuna'] . "'>" . $fila['nombre_comuna'] . "</option>";
 }
 echo $html;
 ConexionBD::cerrarConexion();
